fcef815eb29d7f5ea372eb0cc04bd584e4eaed16
patricia-oliveira <patricia2.oliveira2@gmail.com>
2023-01-29 11:28:59 -0300

    Delete infra directory

--- infra/connection.js
+++
@@ -1,5 +0,0 @@
-//! conectando com o banco de dados
-const path = require('path');
-const { PATH_DB } = require("dotenv").config().parsed;
-const { Database } = require("sqlite3");
-module.exports = new Database(path.resolve() + PATH_DB);
